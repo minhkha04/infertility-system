b0fef51165868badb8f5b02a5f1b230dd1cf4ba0
Le Nguyen Minh Kha <lenguyenminhkha1606@gmail.com>
2025-05-17 14:40:24 +0700

    add login with token

--- src/main/java/com/emmkay/infertility_system_api/controller/AuthenticationController.java
+++ src/main/java/com/emmkay/infertility_system_api/controller/AuthenticationController.java
@@ -1,11 +1,14 @@
 package com.emmkay.infertility_system_api.controller;
 
 import com.emmkay.infertility_system_api.dto.request.AuthenticationRequest;
+import com.emmkay.infertility_system_api.dto.request.IntrospectRequest;
 import com.emmkay.infertility_system_api.dto.request.UserCreationRequest;
 import com.emmkay.infertility_system_api.dto.response.ApiResponse;
 import com.emmkay.infertility_system_api.dto.response.AuthenticationResponse;
+import com.emmkay.infertility_system_api.dto.response.IntrospectResponse;
 import com.emmkay.infertility_system_api.dto.response.UserResponse;
 import com.emmkay.infertility_system_api.service.AuthenticationService;
+import com.nimbusds.jose.JOSEException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
 
 @RestController
 @RequestMapping("/auth")
@@ -23,7 +28,7 @@ public class AuthenticationController {
 
 
     @PostMapping("/login")
-    public ApiResponse<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) {
+    public ApiResponse<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) throws JOSEException {
         return ApiResponse.<AuthenticationResponse>builder()
                 .result(authenticationService.login(request))
                 .build();
@@ -33,6 +38,13 @@ public class AuthenticationController {
     public ApiResponse<UserResponse> register(@RequestBody UserCreationRequest request) {
         return ApiResponse.<UserResponse>builder()
                 .result(authenticationService.register(request))
+                .build();
+    }
+
+    @PostMapping("/introspect")
+    public ApiResponse<IntrospectResponse> introspect(@RequestBody IntrospectRequest request) throws ParseException, JOSEException {
+        return ApiResponse.<IntrospectResponse>builder()
+                .result(authenticationService.introspect(request))
                 .build();
     }
 }
--- src/main/java/com/emmkay/infertility_system_api/service/AuthenticationService.java
+++ src/main/java/com/emmkay/infertility_system_api/service/AuthenticationService.java
@@ -1,8 +1,10 @@
 package com.emmkay.infertility_system_api.service;
 
 import com.emmkay.infertility_system_api.dto.request.AuthenticationRequest;
+import com.emmkay.infertility_system_api.dto.request.IntrospectRequest;
 import com.emmkay.infertility_system_api.dto.request.UserCreationRequest;
 import com.emmkay.infertility_system_api.dto.response.AuthenticationResponse;
+import com.emmkay.infertility_system_api.dto.response.IntrospectResponse;
 import com.emmkay.infertility_system_api.dto.response.UserResponse;
 import com.emmkay.infertility_system_api.entity.Role;
 import com.emmkay.infertility_system_api.entity.User;
@@ -11,12 +13,23 @@ import com.emmkay.infertility_system_api.exception.ErrorCode;
 import com.emmkay.infertility_system_api.mapper.UserMapper;
 import com.emmkay.infertility_system_api.repository.RoleRepository;
 import com.emmkay.infertility_system_api.repository.UserRepository;
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.text.ParseException;
+import java.util.Date;
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -29,9 +42,31 @@ public class AuthenticationService {
     UserMapper userMapper;
     PasswordEncoder passwordEncoder;
 
+    @NonFinal
+    @Value("${jwt.signerKey}")
+    String SIGNER_KEY;
 
-    public AuthenticationResponse login(AuthenticationRequest request) {
-        String mess = "Login successful";
+    String generateToken(User user) throws JOSEException {
+        JWSHeader jwsHeader = new JWSHeader(JWSAlgorithm.HS512);
+        // Create JWT claims set
+        JWTClaimsSet jwsClaimSet = new JWTClaimsSet.Builder()
+                .subject(user.getId())
+                .issuer("emkay.unaux.com")
+                .issueTime(new Date())
+                .expirationTime(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000))
+                .claim("scope", user.getRoleName().getName())
+                .claim("username", user.getUsername())
+                .jwtID(UUID.randomUUID().toString())
+                .build();
+        Payload payload = new Payload(jwsClaimSet.toJSONObject());
+        // Create JWS object
+        JWSObject jwsObject = new JWSObject(jwsHeader, payload);
+        // Sign the JWS object
+        jwsObject.sign(new MACSigner(SIGNER_KEY.getBytes()));
+        return jwsObject.serialize();
+    }
+
+    public AuthenticationResponse login(AuthenticationRequest request) throws JOSEException {
         // find user by username
         User user = userRepository.findByUsername((request.getUsername()))
                 .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
@@ -39,14 +74,12 @@ public class AuthenticationService {
         if (user.getIsRemoved()) {
             throw new AppException(ErrorCode.USER_NOT_ACTIVE);
         }
-
         // check a user's password
         if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
            throw new AppException(ErrorCode.PASSWORD_ERROR);
         }
-
         return AuthenticationResponse.builder()
-                .token(mess)
+                .token(generateToken(user))
                 .build();
     }
 
@@ -62,5 +95,14 @@ public class AuthenticationService {
         userRepository.save(user);
         return userMapper.toUserResponse(user);
 
+    }
+
+    public IntrospectResponse introspect(IntrospectRequest request) throws ParseException, JOSEException {
+        SignedJWT signedJWT = SignedJWT.parse(request.getToken());
+        boolean verified = signedJWT.verify(new MACVerifier(SIGNER_KEY.getBytes()));
+        Date expirationTime = signedJWT.getJWTClaimsSet().getExpirationTime();
+        return IntrospectResponse.builder()
+                .valid(verified && expirationTime.after(new Date()))
+                .build();
     }
 }
