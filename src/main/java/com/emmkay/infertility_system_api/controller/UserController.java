84d171badb8f28f9edee02f74667617b3a1ba315
Le Nguyen Minh Kha <lenguyenminhkha1606@gmail.com>
2025-05-17 16:35:29 +0700

    remove function test in UserController

--- src/main/java/com/emmkay/infertility_system_api/controller/UserController.java
+++ src/main/java/com/emmkay/infertility_system_api/controller/UserController.java
@@ -1,10 +1,6 @@
 package com.emmkay.infertility_system_api.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +8,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
-
-    @PreAuthorize( "hasRole('ADMIN')")
-    @GetMapping("/hello")
-    public String hello() {
-        String id = SecurityContextHolder.getContext().getAuthentication().getName();
-        SecurityContextHolder.getContext().getAuthentication().getAuthorities().forEach(grantedAuthority ->
-                System.out.println(grantedAuthority.getAuthority()));
-        return "Hello " + id;
-    }
 }
