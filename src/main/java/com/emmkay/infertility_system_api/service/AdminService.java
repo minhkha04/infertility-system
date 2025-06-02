8a798f41c0317e2d1b6d1eb4f040e4d2989f0029
Le Nguyen Minh Kha <lenguyenminhkha1606@gmail.com>
2025-06-02 15:14:27 +0700

    modify delete from db

--- src/main/java/com/emmkay/infertility_system_api/service/AdminService.java
+++ src/main/java/com/emmkay/infertility_system_api/service/AdminService.java
@@ -148,6 +148,9 @@ public class AdminService {
     public void deleteUserFromDatabase(String userId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+        if (!user.getIsRemoved()) {
+            throw new AppException(ErrorCode.USER_ALREADY_ACTIVE);
+        }
         userRepository.delete(user);
     }
 }
