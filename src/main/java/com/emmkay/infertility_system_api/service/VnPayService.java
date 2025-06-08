d71624e592a984007638a5dc735592ca450bf3d6
Le Nguyen Minh Kha <lenguyenminhkha1606@gmail.com>
2025-06-08 12:44:32 +0700

    fix time zone in paymentservice

--- src/main/java/com/emmkay/infertility_system_api/service/VnPayService.java
+++ src/main/java/com/emmkay/infertility_system_api/service/VnPayService.java
@@ -53,12 +53,18 @@ public class VnPayService {
         String orderType = "other";
         String vnp_TxnRef = vnPayConfig.getRandomNumber(8) + "TR" + treatmentRecord.getId();
         String vnp_IpAddr = VnPayConfig.getIpAddress(req);
-        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        formatter.setTimeZone(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         String vnp_CreateDate = formatter.format(cld.getTime());
         String vnp_TmnCode = vnPayConfig.getTmnCode();
         cld.add(Calendar.MINUTE, 15);
         String vnp_ExpireDate = formatter.format(cld.getTime());
+
+        System.out.println("CreateDate: " + vnp_CreateDate);
+        System.out.println("ExpireDate: " + vnp_ExpireDate);
+        System.out.println("Local time: " + new Date());
 
         Map<String, String> vnp_Params = new HashMap<>();
         vnp_Params.put("vnp_Version", vnp_Version);
