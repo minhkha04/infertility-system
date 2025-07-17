f6be3ede8e30cca87916a1e2e923000b7cd41b78
Le Nguyen Minh Kha <lenguyenminhkha1606@gmail.com>
2025-07-17 21:02:58 +0700

    add status inprogress for treatmentstep

--- backend/src/main/java/com/emmkay/infertility_system/modules/treatment/enums/TreatmentStepStatus.java
+++ backend/src/main/java/com/emmkay/infertility_system/modules/treatment/enums/TreatmentStepStatus.java
@@ -3,6 +3,7 @@ package com.emmkay.infertility_system.modules.treatment.enums;
 public enum TreatmentStepStatus {
     CONFIRMED,
     COMPLETED,
+    INPROGRESS,
     CANCELLED,
     ;
     public static TreatmentStepStatus fromString(String status) {
